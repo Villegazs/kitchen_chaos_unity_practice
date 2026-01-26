e67d69930c66650eed3a4e13f05faf7bf2705ee4
Villegazs <133931476+Villegazs@users.noreply.github.com>
2026-01-26 17:17:17 -0500

    Add ClearCounter prefab and interaction logic
    Introduces the ClearCounter prefab and its associated script, enabling player interaction via raycasting. Updates the Player script to handle movement with collision checks and interaction with counters, adds a new 'Counters' layer, and configures the scene to include ClearCounter objects.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,101 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    private bool isWalking;
+    
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+    
+    private bool isWalking;
+    private Vector3 lastInteractDir;
     private void Update()
+    {
+        HandleMovement();
+        HandleInteraction();
+    }
+    
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteraction()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+        
+        
+        
+    }
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up* playerHeight, playerRadius, moveDir, moveDistance); //Check if there is a wall in front of the player
+
+        if (!canMove)
+        {
+            //Attempt only x movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                // Can move only on the x
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Cannot move only on the x
+                
+                //Attempt only z movement
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    // Can move only on the z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //Cannot move in any direction
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
         
         isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         
-        Debug.Log(inputVector);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
+        //Debug.Log(inputVector);
     }
 }
