97750d43436c96fac3509af92db6d6e4936fa121
Alejandro Villegas Morales <avmorales2210@gmail.com>
2026-07-30 21:53:29 -0500

    Sync Player Animations

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator : NetworkBehaviour
 {
     private Animator animator;
     private const string IS_WALKING = "IsWalking";
@@ -14,6 +15,8 @@ public class PlayerAnimator : MonoBehaviour
 
     private void Update()
     {
+        if(!IsOwner)
+            return;
         animator.SetBool(IS_WALKING, player.IsWalking());
         
     }
