5fb8327409f8ba23e81aac855ed831582c52d893
Alejandro Villegas Morales <avmorales2210@gmail.com>
2026-08-12 21:18:55 -0500

    Sync plate ingredients over Netcode RPCs
    Route `PlateKitchenObject` ingredient additions through server/client RPCs so all clients update the plate state and fire ingredient-added events consistently. Also replace direct `DestroySelf()` calls in `ClearCounter` with `KitchenObject.DestroyKitchenObject(...)` to use the multiplayer-safe destroy path when transferring ingredients to plates.

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -33,7 +33,7 @@ public class ClearCounter : BaseCounter
                     
                     if(plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
                     {
-                        GetKitchenObject().DestroySelf();
+                        KitchenObject.DestroyKitchenObject(GetKitchenObject());
                     }
                 }
                 else
@@ -44,7 +44,7 @@ public class ClearCounter : BaseCounter
                         //Counter is holding a plate
                        if(plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO()))
                        {
-                           player.GetKitchenObject().DestroySelf();
+                           KitchenObject.DestroyKitchenObject(player.GetKitchenObject());
                        }
                     }
                 }
--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Unity.Netcode;
 
 public class PlateKitchenObject : KitchenObject
 {
@@ -35,11 +36,26 @@ public class PlateKitchenObject : KitchenObject
         }
         else
         {
-            kitchenObjectSOList.Add(kitchenObjectSo);
-            
-            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {kitchenObjectSO = kitchenObjectSo});
+            AddIngredientServerRpc(
+                KitchenGameMultiplayer.Instance.GetKitchenObjectSOIndex(kitchenObjectSo)
+            );
             return true;
         }
+    }
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    private void AddIngredientServerRpc(int kitchenObjectSOIndex)
+    {
+        AddIngredientClientRpc(kitchenObjectSOIndex);
+    }
+    
+    [Rpc(SendTo.Everyone)]
+    private void AddIngredientClientRpc(int kitchenObjectSOIndex)
+    {
+        KitchenObjectSO kitchenObjectSo = KitchenGameMultiplayer.Instance.GetKitchenObjectSOFromIndex(kitchenObjectSOIndex);
+        kitchenObjectSOList.Add(kitchenObjectSo);
+            
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {kitchenObjectSO = kitchenObjectSo});
     }
     
     public List<KitchenObjectSO> GetKitchenObjectSOList()
