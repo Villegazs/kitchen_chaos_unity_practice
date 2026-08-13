c6e7506ca6f6e345a9ddae984c34bdf7545626f8
Alejandro Villegas Morales <avmorales2210@gmail.com>
2026-08-12 21:40:26 -0500

    Add player spawn points, box collider, Players tag
    Position players at per-client spawn points and improve collision handling. Adds spawnPositionList to Player and sets transform.position by OwnerClientId on network spawn; introduces collisionsLayerMask and replaces CapsuleCast checks with BoxCast. Adds a BoxCollider to the Player prefab and sets the prefab layer to 7. Also registers a new 'Players' tag in TagManager and includes necessary using and serialized fields. Note: base.OnNetworkSpawn() call was removed from Player.OnNetworkSpawn.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using System;
 using UnityEngine.EventSystems;
 using Unity.Netcode;
+using System.Collections.Generic;
+
 
 public class Player : NetworkBehaviour, IKitchenObjectParent
 {
@@ -25,7 +27,9 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
     
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private LayerMask countersLayerMask;
+    [SerializeField] private LayerMask collisionsLayerMask;
     [SerializeField] private Transform kitchenObjectHoldPoint;
+    [SerializeField] private List<Vector3> spawnPositionList;
     
     private bool isWalking;
     private Vector3 lastInteractDir;
@@ -47,8 +51,10 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
         {
             LocalInstance = this;
         }
+        
+        transform.position = spawnPositionList[(int)OwnerClientId];
+        
         OnAnyPlayerSpawned?.Invoke(this, EventArgs.Empty);
-        base.OnNetworkSpawn();
     }
 
     private void Start()
@@ -134,13 +140,13 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = .7f;
         float playerHeight = 2f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up* playerHeight, playerRadius, moveDir, moveDistance); //Check if there is a wall in front of the player
+        bool canMove = !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDir, Quaternion.identity, moveDistance, collisionsLayerMask); //Check if there is a wall in front of the player
 
         if (!canMove)
         {
             //Attempt only x movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
-            canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDirX, Quaternion.identity, moveDistance, collisionsLayerMask);
 
             if (canMove)
             {
@@ -153,7 +159,7 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
                 
                 //Attempt only z movement
                 Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
-                canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.BoxCast(transform.position, Vector3.one * playerRadius, moveDirZ, Quaternion.identity, moveDistance, collisionsLayerMask);
                 if (canMove)
                 {
                     // Can move only on the z
